2182bd24e7e881864d8d488b7db63667c16e73ac
KyleHarter <kylenmotion@gmail.com>
2022-05-09 16:44:44 -0400

    Added Invert Channel v0.2.0

--- jsx/KM_setMatteApplier.jsx
+++ jsx/KM_setMatteApplier.jsx
@@ -1,3 +1,14 @@
+/**
+ * Batch set matte applying tool
+ * 
+ * @author: Kyle Harter <k.harter@glassandmarker.com>
+ * @version 0.2.0
+ * 5.9.2022
+ * 
+ * 
+*/
+
+
 var globalLayers = [];
 var globalLayerNames = [];
 
@@ -8,14 +19,24 @@ var mainButtonPanel = mainWindow.add("group", undefined, "Push Me");
 mainButtonPanel.orientation = "row";
 var matteLayerStatic = mainButtonPanel.add("statictext", undefined, "Select Matte Layer:");
 var mainButtonDD = mainButtonPanel.add("dropdownlist", undefined, ["Click Refresh to Populate"]);
-mainButtonDD.selection = 0
-mainButtonDD.size = [160,25]
+mainButtonDD.selection = 0;
+mainButtonDD.size = [160, 25];
+var invertMatte = mainButtonPanel.add("checkbox", undefined, "Invert Matte");
+invertMatte.value = false;
+
+// var extraSettingsPanel = mainWindow.add("panel", undefined,);
+// extraSettingsPanel.orientation = "row";
+// extraSettingsPanel.size = [250, 35];
+// var useForMatteDropDown = extraSettingsPanel.add("dropdownlist", undefined, ["Use For Matte"]);
+// useForMatteDropDown.selection = 0;
+// useForMatteDropDown.size = [160,25]
 
 
 var applyGroup = mainWindow.add("group", undefined, "Push Me");
 applyGroup.orientation = 'row'
-var refreshButton = applyGroup.add("button", undefined, "Refresh List");
-refreshButton.size = [100,40];
+var helpButton = applyGroup.add("button", undefined, "Help Me");
+helpButton.size = [100, 40];
+helpButton.helpTip = "How to use:\n1. Select layer that will serve as matte in the dropdown\n2. Check Invert Matte for Invert Matte\n3. Click apply!";
 var applyButton = applyGroup.add("button", undefined, "Apply Matte");
 applyButton.size = [100, 40];
 
@@ -49,8 +70,10 @@ function init(){
     mainButtonDD.selection = 0;
 }
 
-refreshButton.onClick = function(){
-    init()
+var layerList = init();
+
+helpButton.onClick = function(){
+    alert(helpButton.helpTip)
 }
 
 function getCurrentComp() {
@@ -59,27 +82,31 @@ function getCurrentComp() {
     if (existingComp && existingComp instanceof CompItem) {
         return existingComp
     } else {
-        return alert("Open A Comp Bitch")
+        return alert("Open A Comp")
     }
 }
 
-function applySetMatte(mainButtonDD) {
-    var comp = getCurrentComp()
-    var shapeLayersSelected = comp.selectedLayers;
-    for (var i = 0; i < shapeLayersSelected.length; i++) {
-        var setMatteEffect = shapeLayersSelected[i].property("ADBE Effect Parade").addProperty("ADBE Set Matte3");
-        setMatteEffect.property(1).setValue(parseFloat(mainButtonDD.selection)+1)
+function applySetMatte(mainButtonDD, invertMatte) {
+    var comp = getCurrentComp();
+    var layersSelected = comp.selectedLayers;
+    for (var i = 0; i < layersSelected.length; i++) {
+        var setMatteEffect = layersSelected[i].property("ADBE Effect Parade").addProperty("ADBE Set Matte3");
+        setMatteEffect.property(1).setValue(parseFloat(mainButtonDD.selection) + 1);
+        if (invertMatte.value == false) {
+            setMatteEffect.property(3).setValue(false);
+        } else if (invertMatte.value == true){
+            setMatteEffect.property(3).setValue(true);
+        }
     }
 }
 
 applyButton.onClick = function () {
     app.beginUndoGroup("heckin");
 
-    applySetMatte(mainButtonDD)
+    applySetMatte(mainButtonDD, invertMatte);
     mainWindow.close();
 
     app.endUndoGroup();
 }
-
 
 
