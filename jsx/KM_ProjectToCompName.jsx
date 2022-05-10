572fc8a0991eee34d1fdc3d9609ba9b66a52cc17
KyleHarter <kylenmotion@gmail.com>
2022-05-10 17:52:32 -0400

    v2 - project full name of script compatible

--- jsx/KM_ProjectToCompName.jsx
+++ jsx/KM_ProjectToCompName.jsx
@@ -2,17 +2,20 @@
  * Renames Comp to project name
  * @title KM_ProjectToCompName
  * @author Kyle Harter <k.harter@glassandmarker.com>
- * @version 0.2.0
+ * @version 0.2.1
  * 5.10.2022
  * 
  * 
 */
 
-(function projectToCompName(){
-    function getSelection() {
+(function projectToCompName() {
+
+
+
+    function getComp() {
         var selectedComps = app.project.selection
         var compArray = new Array()
-        for (var i = 0; i <= selectedComps.length; i++){
+        for (var i = 0; i <= selectedComps.length; i++) {
             var compSelection = selectedComps[i];
             if (compSelection && compSelection instanceof CompItem) {
                 compArray.push(compSelection)
@@ -26,7 +29,10 @@
         }
     }
 
-    function renameComp2ProjName() {
+    var compName = getComp();
+
+
+    function getFullProjectName() {
         var projectName = "Untitled";
         if (app.project.file != null) {
             projectName = app.project.file.name;
@@ -35,18 +41,40 @@
         if (projectName.indexOf(".") != -1) { // if project has a prefix (e.g. .aep) strip it
             projectName = projectName.substring(0, projectName.lastIndexOf("."));
         }
-        var selectionName = getSelection();;
-        if (selectionName != null) {
-            return getSelection().name = projectName.replace(/%20/g, " ");
-        } else {
-            return null
-        }
+
+        return projectName.replace(/%20/g, " ")
+
     }
+
+    var projectName = getFullProjectName();
+
+
+    function removeProjCode(projectName) {
+        var projCode = projectName.split("-")[0];
+        var trimmedProjName = projectName.replace(new RegExp(projCode, "g"), "");
+        var removeDash = trimmedProjName.replace(/^\W/gi, "");
+
+         return removeDash
+        
+    }
+
+    var projNameNoProjCode = removeProjCode(projectName);
+
+
 
     app.beginUndoGroup("Rename Composition Project Name");
 
-
-    renameComp2ProjName()
+    var shiftHeld = ScriptUI.environment.keyboardState.shiftKey;
+    if (shiftHeld) {
+        return compName.name = projectName;
+    } else {
+        if (projNameNoProjCode == "") {
+            alert("No Project Code Present. Shift+Click script for full project name")
+            return
+        } else {
+            return compName.name = projNameNoProjCode
+        }
+    }
 
     app.endUndoGroup()
     
