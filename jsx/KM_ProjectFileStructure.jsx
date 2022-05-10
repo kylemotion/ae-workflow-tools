295c8d4cec5059fc34a280a071bb42db7cd7b994
KyleHarter <kylenmotion@gmail.com>
2022-05-10 11:58:55 -0400

    Updated Project Folder Script

--- jsx/KM_ProjectFileStructure.jsx
+++ jsx/KM_ProjectFileStructure.jsx
@@ -11,53 +11,33 @@
 
 (function projectStructure(){
     /// Global Variables
-    var proj = app.project.items;
+    var proj = app.project;
+    var projItems = proj.items;
     
     /// Folder Names
     var rootFolders = ["01 Main Comps", "02 PreComps", "03 Imported"];
+    var importedSubFolders = ["Projects", "Footage", "Graphics", "Intermediary"];
 
-    /// Run Script
-        app.beginUndoGroup("Folder Structure");
+    function createRootFolders(rootFolders, importedSubFolders) {
+        var mainComp = projItems.addFolder(rootFolders[0]);
+        var preComp = projItems.addFolder(rootFolders[1]);
+        var importedFolder = projItems.addFolder(rootFolders[2]);
 
-    alert(createFolders(proj, rootFolders))
-
-        app.endUndoGroup();
-
-
-    function createFolders(proj, rootFolders) {
-        var itemArray = new Array();
-        var folderArray = new Array();
-        for (var i = 1; i <= app.project.numItems; i++) {
-            itemArray.push(app.project.item(i));
-            for (var b = 0; b < rootFolders.length; b++){
-                if (itemArray[i-1].indexOf(rootFolders[b]) != -1) {
-                    folderArray.push(itemArray[i])
-                    $.writeln(itemArray[i].name)
-                }
-            }
+        for (var i = 0; i < importedSubFolders.length; i++){
+            var subFolders = projItems.addFolder(importedSubFolders[i]);
+            subFolders.parentFolder = importedFolder
         }
-        return folderArray.toString()
 
-        // var mainCompsFolder = proj.item("01 Main Comps");
-        // if (!mainCompsFolder) {
-        //     var mainCompsFolder = proj.addFolder("01 Main Comps");
-        // } 
-
-        // if (!preCompsFolder) {
-        //     var preCompsFolder = proj.addFolder("02 PreComps");
-        // }
-        
-        // if (!importedFolder) {
-        //     var importedFolder = proj.addFolder("03 Imported");
-        // }
-
-        // if (importedFolder) {
-        //     var importedArray = new Array("Projects", "Footage", "Graphics", "Intermediary")
-        //     for (var i = 0; i < importedArray.length; i++) {
-        //         var importedSubFolders = proj.addFolder(importedArray[i]);
-        //         importedSubFolders.parentFolder = importedFolder
-        //     }
-        // }
     }
 
+    /// Run Script
+    app.beginUndoGroup("Folder Structure");
+
+    createRootFolders(rootFolders, importedSubFolders)
+
+    app.endUndoGroup();
+
 })()
+
+
+    // && proj.item(i).name === rootFolders[i - 1].name
