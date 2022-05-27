4ed09246d73645e21998b52cb4bcdf9f8dca1652
KyleHarter <kylenmotion@gmail.com>
2022-05-27 11:29:34 -0400

    added viz option cleaned up code documented

--- jsx/KM_setMatteApplier.jsx
+++ jsx/KM_setMatteApplier.jsx
@@ -1,53 +1,121 @@
 /**
  * Batch set matte applying tool
+ * can invert matte
+ * can also turn matte on/off
+ * dockable: yes
+ * 
  * 
  * @title Set Matte Applier
  * @author Kyle Harter <k.harter@glassandmarker.com>
- * @version 0.2.1
- * 5.9.2022
+ * @version 0.2.2
+ * 5.27.2022
  * 
  * 
 */
 
-(function km_setMatteApplier() {
+(function km_setMatteApplier(thisObj) {
+    
+    
+    var existingComp = app.project.activeItem;
+
+    if (!(existingComp && existingComp instanceof CompItem)) {
+        alert("Open a comp first!")
+        return
+    } 
+
+
+    if (existingComp.selectedLayers == 0) {
+        alert("Select atleast one layer to apply the Set Matte effect to")
+        return
+    }
+
+
+    var globalLayers = [];
+    var globalLayerNames = [];
     
 
-var globalLayers = [];
-var globalLayerNames = [];
+    createUI(thisObj);
 
-var mainWindow = new Window("window", "Set Matte Applier", undefined);
-mainWindow.orientation = "column";
-mainWindow.alignChildren = ["center", "center"];
+    /**
+     * 
+     * @param {Object} thisObj activates UI immediately
+     * @returns ui
+     */
 
-var setMatteSettingsPanel = mainWindow.add("panel", undefined, "Set Matte Settings");
-setMatteSettingsPanel.orientation = "column";
-setMatteSettingsPanel.alignChildren = ["left", "fill"]
-setMatteSettingsPanel.margins = 15;
-    var matteLayerStatic = setMatteSettingsPanel.add("statictext", undefined, "Select Matte Layer:");
+    function createUI(thisObj) {
+        var win = thisObj instanceof Panel
+            ? thisObj
+            : new Window("window", "km_setMatteApplier", undefined, {
+                resizeable: true
+            })
+        
     
-var mainButtonDD = setMatteSettingsPanel.add("dropdownlist", [0,0,160,25], ["Click Refresh to Populate"]);
-mainButtonDD.selection = 0;
-var invertMatte = setMatteSettingsPanel.add("checkbox", undefined, "\u00A0Invert Matte");
-invertMatte.value = false;
+        var mainWindow = win.add("group", undefined, "main window");
+        mainWindow.orientation = "column";
+        mainWindow.alignChildren = ["center", "center"];
 
-var applyGroup = mainWindow.add("group", undefined, "Push Me");
-    applyGroup.orientation = 'row';
-    applyGroup.alignChildren = "fill";
-var helpButton = applyGroup.add("button", undefined, "Help Me");
-// helpButton.size = [25, 25];
-helpButton.helpTip = "How to use:\n1. Select layer that will serve as matte in the dropdown\n2. Check Invert Matte for Invert Matte\n3. Click apply!";
-var applyButton = applyGroup.add("button", undefined, "Apply Matte");
-// applyButton.size = [100, 25];
+        var setMatteSettingsPanel = mainWindow.add("panel", undefined, "Set Matte Settings");
+        setMatteSettingsPanel.orientation = "column";
+        setMatteSettingsPanel.alignChildren = ["left", "fill"]
+        setMatteSettingsPanel.margins = 15;
+            var matteLayerStatic = setMatteSettingsPanel.add("statictext", undefined, "Select Matte Layer:");
+            
+        var mainButtonDD = setMatteSettingsPanel.add("dropdownlist", [0,0,160,25], ["Click Refresh to Populate"]);
+        mainButtonDD.selection = 0;
 
-mainWindow.center();
-mainWindow.show();
+        var matteOptionsGroup = setMatteSettingsPanel.add("group", undefined, "matte options");
+            
+        var invertMatte = matteOptionsGroup.add("checkbox", undefined, "\u00A0Invert Matte");
+        invertMatte.value = false;
+        var matteVisibility = matteOptionsGroup.add("checkbox", undefined, "\u00A0Matte Visible");
+        matteVisibility.value = true;
+
+        var applyGroup = mainWindow.add("group", undefined, "Push Me");
+            applyGroup.orientation = 'row';
+            applyGroup.alignChildren = "fill";
+        var helpButton = applyGroup.add("button", undefined, "Help Me");
+        helpButton.helpTip = "How to use:\n1. Select layer that will serve as matte in the dropdown\n\n2. Check Invert Matte for Invert Matte\n\n3. Click Matte Visible to change matte visibility\n\n4. Click apply!";
+        var applyButton = applyGroup.add("button", undefined, "Apply Matte");
+
+
+        win.layout.layout();
+        win.onResizing = function () {
+            this.layout.resize();
+        };
+
+        
+
+        var layerList = init(mainButtonDD);
 
 
 
-function init(){
+        helpButton.onClick = function(){
+            alert(helpButton.helpTip)
+        }
+
+        applyButton.onClick = function () {
+
+            app.beginUndoGroup("apply set matte");
+            
+        applySetMatte(mainButtonDD, invertMatte.value, matteVisibility.value);
+            win.close();
+
+        app.endUndoGroup();
+}
+
+        win.show()
+    }
+
+/**
+ * 
+ * @param {Object} dropDown dropdown that holds list of layers in comp
+ * @returns first drop down selection item
+ */
+
+    function init(dropDown){
     globalLayers = [];
     globalLayerNames = [];
-    mainButtonDD.removeAll();
+    dropDown.removeAll();
     
     var existingComp = app.project.activeItem;
     
@@ -55,57 +123,48 @@ function init(){
         for(var i = 1; i<=existingComp.numLayers; i++){
             globalLayers.push(existingComp.layer(i));
             globalLayerNames.push(i.toString() + ". " + existingComp.layer(i).name);
-            mainButtonDD.add("item", globalLayerNames[globalLayerNames.length-1])
+            dropDown.add("item", globalLayerNames[globalLayerNames.length-1])
 
             if(existingComp.layer(i) instanceof AVLayer && existingComp.layer(i).canSetCollapseTransformation){
                 existingComp.layer(i).collapseTransformation = true
             }
 
         }
-    } else {
-        alert("Select Your Composition First")
-    }
+    } 
     
-    mainButtonDD.selection = 0;
+    return dropDown.selection = 0;
 }
-
-var layerList = init();
-
-helpButton.onClick = function(){
-    alert(helpButton.helpTip)
-}
-
-function getCurrentComp() {
-    var existingComp = app.project.activeItem;
-
-    if (existingComp && existingComp instanceof CompItem) {
-        return existingComp
-    } else {
-        return alert("Open A Comp")
-    }
-}
-
-function applySetMatte(mainButtonDD, invertMatte) {
-    var comp = getCurrentComp();
+    
+    /**
+     * 
+     * @param {Object} dropDown dropdown list of layers
+     * @param {Object} invertMatte invert matte checkbox
+     * @param {Object} matteViz matte visibility checkbox
+     * @returns apply set matte process
+     */
+    
+    function applySetMatte(dropDown, invertMatte, matteViz) {
+    var comp = existingComp;
     var layersSelected = comp.selectedLayers;
-    for (var i = 0; i < layersSelected.length; i++) {
+        for (var i = 0; i < layersSelected.length; i++) {
+            var ddSel = parseFloat(dropDown.selection) + 1;
+
         var setMatteEffect = layersSelected[i].property("ADBE Effect Parade").addProperty("ADBE Set Matte3");
-        setMatteEffect.property(1).setValue(parseFloat(mainButtonDD.selection) + 1);
-        if (invertMatte.value == false) {
+            setMatteEffect.property(1).setValue(ddSel);
+        if (invertMatte == false) {
             setMatteEffect.property(3).setValue(false);
-        } else if (invertMatte.value == true){
+        } else if (invertMatte == true){
             setMatteEffect.property(3).setValue(true);
+        }
+
+        
+        if (matteViz == true) {
+            comp.layer(ddSel).enabled = true;
+        } else if (matteViz == false) {
+            comp.layer(ddSel).enabled = false;
         }
     }
 }
 
-applyButton.onClick = function () {
-    app.beginUndoGroup("heckin");
 
-    applySetMatte(mainButtonDD, invertMatte);
-    mainWindow.close();
-
-    app.endUndoGroup();
-}
-
-})()
+})(this)
