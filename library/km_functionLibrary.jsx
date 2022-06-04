71e666ba6068eabb183f3ab7b9975f218f614d3b
KyleHarter <kylenmotion@gmail.com>
2022-06-04 14:29:11 -0400

    updated function library

--- library/km_functionLibrary.jsx
+++ library/km_functionLibrary.jsx
@@ -21,3 +21,35 @@ function hexToRGB(hexValue) {
 
     return [r / 255, g / 255, b / 255]
 } 
+
+
+//// Dockable or window ui
+
+
+(function (thisObj) {
+    buildUI(thisObj);
+
+    function buildUI(thisObj) {
+        var win = (thisObj instanceof Panel) ? thisObj : new Window("palette", "script", undefined, {
+            resizeable: true
+        });
+
+        var button = win.add('button', undefined, 'Click me');
+        button.onClick = function () {
+            alert('Hello World');
+        };
+
+        win.onResizing = win.onResize = function () {
+            this.layout.resize();
+        };
+
+        if (win instanceof Window) {
+            win.center();
+            win.show();
+        } else {
+            win.layout.layout(true);
+            win.layout.resize();
+        }
+    }
+
+})(this);
