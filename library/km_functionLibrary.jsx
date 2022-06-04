07a17196197996268d3203e335821e56aa7fde1c
KyleHarter <kylenmotion@gmail.com>
2022-06-04 08:29:49 -0400

    Updated hexToRGB function

--- library/km_functionLibrary.jsx
+++ library/km_functionLibrary.jsx
@@ -10,10 +10,14 @@
  */
 
 
-function hexToRGB(mainColor) {
-    var hexColor = "0x" + mainColor;
+function hexToRGB(hexValue) {
+    var hexTrim = hexValue.trim();
+    var hexString = hexTrim;
+    var finalHex = hexString.replace(/[#]/g, "");
+    var hexColor = "0x" + finalHex;
     var r = hexColor >> 16;
     var g = (hexColor & 0x00ff00) >> 8;
     var b = hexColor & 0xff;
-    return [r / 255, g / 255, b / 255];
-}
+
+    return [r / 255, g / 255, b / 255]
+} 
